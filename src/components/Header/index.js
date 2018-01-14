57e7feea1bfc93c9fc12bd640b04eb53061e8f5b
Shuhei Nomura <info@danmaq.com>
2018-01-15 00:20:30 +0900

    Added navbar.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,13 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-const Header = () => (
-    <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
+import logonInv from '../../assets/logo/logoInv.svg';
+
+export default () =>
+<div>
+    <nav className="navbar navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+            <img src={logonInv} width="282" height="100" alt="danmaq" />
         </Link>
-      </h1>
-    </div>
-  </div>
-)
-
-export default Header
+    </nav>
+</div>;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+import './index.css';
 
-const TemplateWrapper = ({ children }) => (
-    <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+export default class extends React.Component {
+    render =
+        () =>
+        (({ children }) =>
+            <div>
+                <Helmet defaultTitle="danmaq" titleTemplate="%s | danmaq">
+                    <html lang="ja" />
+                    <link rel="author"
+                          href="https://twitter.com/danmaq"
+                          hrefLang="ja" />
+                    <link rel="stylesheet"
+                          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"
+                          integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy"
+                          crossorigin="anonymous" />
+                    <meta name="description"
+                          content="danmaq は、東方・ボカロおよびオリジナルを中心に、ゲームなど様々なコンテンツを提供する同人ブランドです。" />
+                    <meta name="keywords"
+                          content="Touhou CtC, Concealed the Conclusion" />
+                </Helmet>
+                <Header />
+                <div style={{
+                    margin: '0 auto',
+                    maxWidth: 960,
+                    padding: '0px 1.0875rem 1.45rem',
+                    paddingTop: 0 }} >
+                {children()}
+                </div>
+            </div>
+        )(this.props);
 
-TemplateWrapper.propTypes = {
-    children: PropTypes.func,
-}
-
-export default TemplateWrapper
+    static propTypes = { children: PropTypes.func };
+};
