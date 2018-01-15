9ca29c6ad09bd7bcc7e0770379e956ff65843ef2
Shuhei Nomura <info@danmaq.com>
2018-01-15 23:05:12 +0900

    Implemented header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,47 @@
+'use strict';
+
 import React from 'react';
 import Link from 'gatsby-link';
+import { Nav, Navbar, NavbarBrand, NavItem } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 import logonInv from '../../assets/logo/logoInv.svg';
 
+/** Common header component. */
 export default () =>
 <div>
-    <nav className="navbar navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
-            <img src={logonInv} width="282" height="100" alt="danmaq" />
-        </Link>
-    </nav>
+    <Navbar collapseOnSelect fixedTop inverse staticTop>
+        <Navbar.Header>
+            <Link className="pull-left" to="/">
+                <img src={logonInv} width="282" height="100" alt="danmaq" />
+            </Link>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <NavItem eventKey={1} href="#works">Works</NavItem>
+                <NavItem eventKey={2} href="#about">About</NavItem>
+                <NavItem eventKey={3} href="#contact">Contact</NavItem>
+                <NavItem eventKey={4} href="#blog">Blog</NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavItem eventKey={5}
+                         href="https://github.com/danmaq"
+                         target="_blank">
+                    <FontAwesome name="github"
+                                 size="2x"
+                                 fixedWidth
+                                 inverse />
+                </NavItem>
+                <NavItem eventKey={6}
+                         href="https://twitter.com/danmaq"
+                         target="_blank">
+                    <FontAwesome name="twitter"
+                                 size="2x"
+                                 fixedWidth
+                                 inverse />
+                </NavItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 </div>;
