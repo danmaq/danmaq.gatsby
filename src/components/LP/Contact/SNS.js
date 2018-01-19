437bae6184e3f082274b2ea8caeb5dc27da0daff
Shuhei Nomura <info@danmaq.com>
2018-01-19 15:55:31 +0900

    Improved that color of SNS icons.

--- src/components/LP/Contact/SNS.js
+++ src/components/LP/Contact/SNS.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Heading from '../Heading';
 
-import { Button, Column, Columns, Image } from 'bloomer';
+import { Button, Column, Columns, Container, Image } from 'bloomer';
 
 import Icon from '~/src/components/Icon';
 
@@ -16,106 +16,50 @@ export default () =>
     <Heading caption="SNS でも最新情報を受け取りましょう"
              detail="主に Twitter や GitHub を中心に活動しています。最新の活動状況も Twitter で配信中です。"
              sub />
-    <Columns className="dmq-sns">
-        <Column>
-            <Columns isMobile>
-                <Column hasTextAlign="centered">
-                    <Button href="https://twitter.com/danmaq"
-                            target="_blank"
-                            isSize="large"
-                            isLink>
-                        <Icon i="twitter" size={3} />
-                    </Button>
-                </Column>
-                <Column hasTextAlign="centered">
-                    <Button href="https://github.com/danmaq"
-                            target="_blank"
-                            isSize="large"
-                            isLink>
-                        <Icon i="github" size={3} />
-                    </Button>
-                </Column>
-            </Columns>
-        </Column>
-        <Column>
-            <Columns isMobile>
-                <Column hasTextAlign="centered">
-                    <Button href="http://com.nicovideo.jp/community/co2711221"
-                            target="_blank"
-                            isSize="large"
-                            isLink>
-                        <Image isSize="96x96"
-                               src={Niconico} />
-                    </Button>
-                </Column>
-                <Column hasTextAlign="centered">
-                    <Button href="https://twitter.com/danmaq"
-                            target="_blank"
-                            isSize="large"
-                            isLink>
-                        <Image isSize="96x96"
-                               src={Pixiv} />
-                    </Button>
-                </Column>
-            </Columns>
-        </Column>
-    </Columns>
+    <Container>
+        <Columns className="dmq-sns">
+            <Column>
+                <Columns isMobile>
+                    <Column hasTextAlign="centered">
+                        <Button className="is-twitter"
+                                href="https://twitter.com/danmaq"
+                                target="_blank"
+                                isSize="large">
+                            <Icon i="twitter" size={4} />
+                        </Button>
+                    </Column>
+                    <Column hasTextAlign="centered">
+                        <Button className="is-github"
+                                href="https://github.com/danmaq"
+                                target="_blank"
+                                isSize="large">
+                            <Icon i="github" size={4} />
+                        </Button>
+                    </Column>
+                </Columns>
+            </Column>
+            <Column>
+                <Columns isMobile>
+                    <Column hasTextAlign="centered">
+                        <Button className="is-nico"
+                                href="http://com.nicovideo.jp/community/co2711221"
+                                target="_blank"
+                                isSize="large">
+                            <Image isSize="96x96"
+                                   src={Niconico} />
+                        </Button>
+                    </Column>
+                    <Column hasTextAlign="centered">
+                        <Button className="is-pixiv"
+                                href="https://twitter.com/danmaq"
+                                target="_blank"
+                                isSize="large">
+                            <Image isSize="96x96"
+                                   src={Pixiv} />
+                        </Button>
+                    </Column>
+                </Columns>
+            </Column>
+        </Columns>
+    </Container>
 </div>;
-
-/*
-    <Grid>
-        <Row>
-            <Col className="dmq-sns" xs={6} md={3}>
-                <Button href="https://twitter.com/danmaq"
-                        target="_blank"
-                        bsStyle="primary"
-                        bsSize="large">
-                    <FontAwesome name="twitter"
-                                 size="4x"
-                                 title="Twitter"
-                                 fixedWidth
-                                 inverse />
-                </Button>
-            </Col>
-            <Col className="dmq-sns" xs={6} md={3}>
-                <Button href="https://github.com/danmaq"
-                        target="_blank"
-                        bsStyle="primary"
-                        bsSize="large">
-                    <FontAwesome name="github"
-                                 size="4x"
-                                 title="GitHub"
-                                 fixedWidth
-                                 inverse />
-                </Button>
-            </Col>
-            <Col className="dmq-sns" xs={6} md={3}>
-                <Button href="http://com.nicovideo.jp/community/co2711221"
-                        target="_blank"
-                        bsStyle="primary"
-                        bsSize="large">
-                    <Image alt="niconico"
-                           src={Niconico}
-                           title="niconico"
-                           width={64}
-                           height={64}
-                           rounded />
-                </Button>
-            </Col>
-            <Col className="dmq-sns" xs={6} md={3}>
-                <Button href="https://pixiv.me/danmaq"
-                        target="_blank"
-                        bsStyle="primary"
-                        bsSize="large">
-                    <Image alt="pixiv"
-                           src={Pixiv}
-                           title="pixiv"
-                           width={64}
-                           height={64}
-                           rounded />
-                </Button>
-            </Col>
-        </Row>
-    </Grid>
-
-*/
