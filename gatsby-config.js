344bc3c5a846c04ae43ddac5212358719966b49b
Shuhei Nomura <info@danmaq.com>
2018-01-20 20:52:32 +0900

    Added dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-react-next',
         `gatsby-plugin-sass`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        }
     ],
     siteMetadata: { title: 'danmaq' },
 };
