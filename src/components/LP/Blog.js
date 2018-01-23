7d44e3807d656e0a5c657f5b431f48279a45d4bd
Shuhei Nomura <info@danmaq.com>
2018-01-23 16:07:50 +0900

    Refactored.

--- src/components/LP/Blog.js
+++ src/components/LP/Blog.js
@@ -3,17 +3,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-import { Column, Columns, Content, Section } from 'bloomer';
+import { Columns, Content, Section } from 'bloomer';
 
 import Article from '~/src/components/Article';
 import Icon from '~/src/components/Icon';
 
 import Heading from './Heading';
-
-import Comiket from '~/src/assets/LP/photo/comiket.jpg';
-import Enna from '~/src/assets/LP/photo/enna.jpg';
-import ReactNative from '~/src/assets/LP/icon/react.svg';
-import Workshop from '~/src/assets/LP/photo/workshop.jpg';
 
 export default class extends React.Component {
     render =
@@ -21,13 +16,13 @@ export default class extends React.Component {
         (({ items }) =>
             <Section id="blog">
                 <Heading caption="Blog"
-                    detail="直近 3 件の新着ブログ記事を掲載します。" />
+                         detail="直近 3 件の新着ブログ記事を掲載します。" />
                 <Columns isCentered isMultiline>
                     {items.map(Article.create)}
                 </Columns>
                 <Content>
                     <Link className='button is-fullwidth is-large is-outlined is-link'
-                        to="/blog">
+                          to="/blog">
                         <Icon i="angle-down" size={2} />
                         &nbsp;さらに過去のブログ記事も見る
                     </Link>
