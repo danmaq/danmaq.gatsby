c11f1833ec2387a6fa3d469f4ee959f3dff9853c
Shuhei Nomura <info@danmaq.com>
2018-01-24 19:17:21 +0900

    Moved submodule place.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 'use strict';
 
 module.exports = {
-    pathPrefix: "/danmaq.gatsby",
+    pathPrefix: '/danmaq.gatsby',
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-react-next',
-        `gatsby-plugin-sass`,
-        `gatsby-transformer-remark`,
+        'gatsby-plugin-sass',
+        'gatsby-transformer-remark',
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                name: `posts`,
-                path: `${__dirname}/src/danmaq.article/posts`,
+                excerpt_separator: `<!-- end -->`,
+                name: 'posts',
+                path: '${__dirname}/danmaq.article/posts',
             },
         }
     ],
