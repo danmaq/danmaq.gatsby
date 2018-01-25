584450ecaf9ee17f0b9688dcb5b15693f5c20894
Shuhei Nomura <info@danmaq.com>
2018-01-25 19:45:46 +0900

    Added dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,24 +2,33 @@
 
 module.exports = {
     pathPrefix: '/danmaq.gatsby',
-    plugins: [
+    plugins: [{
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                excerpt_separator: '<!-- more -->',
+                name: 'posts',
+                path: `${__dirname}/danmaq.article/posts/`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [{
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                            linkImagesToOriginal: true,
+                        },
+                    },
+                    'gatsby-remark-copy-linked-files',
+                ],
+            },
+        },
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-react-next',
         'gatsby-plugin-sass',
-        'gatsby-transformer-remark',
-        {
-            options: {
-                excerpt_separator: `<!-- end -->`,
-                name: 'posts',
-                path: `${__dirname}/danmaq.article/posts`,
-            },
-            plugins: [{
-                ignoreFileExtensions: [],
-                options: { destinationDir: '.' },
-                resolve: 'gatsby-remark-copy-linked-files',
-            }],
-            resolve: 'gatsby-source-filesystem',
-        }
     ],
     siteMetadata: { title: 'danmaq' },
 };
