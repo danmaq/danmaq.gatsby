b8d6294e9e3331d8a53a6b627c3e7a2e58121401
Shuhei Nomura <info@danmaq.com>
2018-02-04 01:45:55 +0900

    Improved caption image.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,8 +9,10 @@ import {
     Content,
     Hero,
     HeroBody,
+    HeroFooter,
     Title,
 } from 'bloomer';
+import _ from 'lodash';
 
 import Article from '~/src/components/Article';
 import Header from '~/src/components/Header';
@@ -24,7 +26,7 @@ export default ({
     data: {
         markdownRemark: {
             html,
-            frontmatter: { title, date, strDate }
+            frontmatter: { cover, date, strDate, title }
         }
     },
     pathContext,
@@ -34,17 +36,25 @@ export default ({
         <title>{title}</title>
     </Helmet>
     <Header pathContext={pathContext} />
-    <Hero>
+    <Hero isSize="medium">
         <HeroBody>
             <Container>
                 <Title isSize={2} tag="h1">{title}</Title>
             </Container>
         </HeroBody>
-    </Hero>
+        <HeroFooter>
+            <aside className="container">
+                投稿日時: <time dateTime={date}>{strDate}</time>
+            </aside>
+        </HeroFooter>
+        </Hero>
     <section className="container">
-        <aside className="container">
-            投稿日時: <time dateTime={date}>{strDate}</time>
-        </aside>
+        <figure className="image">
+            <img alt=""
+                 sizes={_.get(cover, 'childImageSharp.responsiveSizes.sizes')}
+                 src={_.get(cover, 'childImageSharp.responsiveSizes.src')}
+                 srcSet={_.get(cover, 'childImageSharp.responsiveSizes.srcSet')} />
+        </figure>
         <Columns isCentered>
             <Column isHidden="touch" isSize={1} />
             <Column isSize={10}>
@@ -55,40 +65,6 @@ export default ({
             <Column isHidden="touch" isSize={1} />
         </Columns>
     </section>
-    <Hero>
-        <HeroBody>
-            <Container hasTextAlign='centered'>
-                <Title isSize={3} tag="h2">関連記事</Title>
-            </Container>
-        </HeroBody>
-    </Hero>
-    <Container>
-        <Columns isMultiline>
-            <Article image={Enna}
-                     date="2017-12-22T00:00:00+09:00"
-                     strDate="2017/12/22"
-                     href="blog"
-                     caption="Genesis 1"
-                     detail="In the beginning God created the heaven and the earth. And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters." />
-            <Article image={Comiket}
-                     date="2017-12-22T00:00:00+09:00"
-                     strDate="2017/12/22"
-                     href="blog"
-                     caption="件名"
-                     detail="The quick brown fox jumps over the lazy dog." />
-            <Article href="blog"
-                     date="2017-12-22T00:00:00+09:00"
-                     strDate="2017/12/22"
-                     caption="Card"
-                     detail="Some quick example text to build on the card title and make up the bulk of the card's content." />
-            <Article image={ReactNative}
-                     date="2017-12-22T00:00:00+09:00"
-                     strDate="2017/12/22"
-                     href="blog"
-                     caption="React のやべーやつ"
-                     detail="寿限無 寿限無 五劫の擦り切れ 海砂利水魚の 水行末 雲来末 風来末 食う寝る処に住む処 藪ら柑子の藪柑子 パイポ パイポ パイポのシューリンガン シューリンガンのグーリンダイ グーリンダイのポンポコピーのポンポコナーの 長久命の長助" />
-        </Columns>
-    </Container>
 </div>;
 
 export const query =
