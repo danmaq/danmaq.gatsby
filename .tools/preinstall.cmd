f051ddb0ca13824bd7fb3bf1413c1c410ec0dca6
Shuhei Nomura <info@danmaq.com>
2018-03-07 09:58:43 +0900

    Implemented skip pre-install when detect pre-installed.

--- .tools/preinstall.cmd
+++ .tools/preinstall.cmd
@@ -1,9 +1,18 @@
-@ECHO OFF
-whoami /groups | Find "High Mandatory Level" > NUL
-IF not ERRORLEVEL 1 GOTO RUN
+@echo off
+for /f "usebackq tokens=*" %%i in (`yarn global dir`) DO @set YARN_GLOBAL=%%i
+cd %YARN_GLOBAL%
+call yarn list | find "windows-build-tools" > NUL
+if not ERRORLEVEL 0 goto DOIT
+call yarn outdated | find "windows-build-tools" > NUL
+if ERRORLEVEL 0 goto DOIT
+exit /B 0
+
+:DOIT
+whoami /groups | find "High Mandatory Level" > NUL
+if not ERRORLEVEL 1 goto RUN
 powershell -Command Start-Process -Wait -Verb runas "%~0"
-EXIT /b %ERRORLEVEL%
+exit /B %ERRORLEVEL%
 :RUN
 
-WHERE /Q yarn && CALL yarn global add windows-build-tools
-WHERE /Q yarn || CALL npm i -g windows-build-tools
+where /Q yarn && call yarn global add windows-build-tools
+where /Q yarn || call npm i -g windows-build-tools
