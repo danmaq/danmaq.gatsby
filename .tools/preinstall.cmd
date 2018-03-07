391683476593e637a97e9afa1f4236924a28dddc
Shuhei Nomura <info@danmaq.com>
2018-03-07 13:52:22 +0900

    Fixed bugs in preinstall for Windows.

--- .tools/preinstall.cmd
+++ .tools/preinstall.cmd
@@ -1,15 +1,18 @@
 @echo off
+setlocal
 for /f "usebackq tokens=*" %%i in (`yarn global dir`) DO @set YARN_GLOBAL=%%i
 cd %YARN_GLOBAL%
 call yarn list | find "windows-build-tools" > NUL
-if not ERRORLEVEL 0 goto DOIT
+if %ERRORLEVEL% neq 0 goto DOIT
 call yarn outdated | find "windows-build-tools" > NUL
-if ERRORLEVEL 0 goto DOIT
+if %ERRORLEVEL% equ 0 goto DOIT
+endlocal
 exit /B 0
 
 :DOIT
+endlocal
 whoami /groups | find "High Mandatory Level" > NUL
-if not ERRORLEVEL 1 goto RUN
+if %ERRORLEVEL% equ 0 goto RUN
 powershell -Command Start-Process -Wait -Verb runas "%~0"
 exit /B %ERRORLEVEL%
 :RUN
